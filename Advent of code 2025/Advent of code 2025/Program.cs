c6c855d1243053cf56775b5c5ef55d3b45d7528c
Daniel White <danielwhite1722@gmail.com>
2025-12-02 09:17:54 +0000

    Day 1  - part 1 complete, part 2 incomplete

---
+++ Advent of code 2025/Advent of code 2025/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FILEName = "Input.txt";
+            StreamReader SR = new StreamReader(FILEName);
+            string temp = SR.ReadToEnd();
+            temp = temp.Replace("\r", "");
+            string[] AllString = temp.Split('\n');
+            int ValueofDial = 50;
+            int Password = 0;
+            foreach (string s in AllString)
+            {
+                string LorR = s.Substring(0, 1);
+                string Rotation = s.Substring(1);
+                int Amount = int.Parse(Rotation);
+                if (LorR == "L")
+                {
+                    if (ValueofDial - Amount < 0)
+                    {
+                        ValueofDial = 100 + (ValueofDial - Amount);
+                        Password++;
+                        while (ValueofDial < 0)
+                        {
+                            Password++;
+                            ValueofDial = 100 + ValueofDial;
+                        }
+                        
+                    }
+                    else ValueofDial = ValueofDial - Amount;
+                }
+                else
+                {
+                    if (ValueofDial + Amount > 99)
+                    {
+                        Password++;
+                        ValueofDial = (ValueofDial + Amount) - 100;
+                        while (ValueofDial > 99)
+                        {
+                            Password++;
+                            ValueofDial = ValueofDial - 100;
+                        }
+                    }
+                    else ValueofDial = ValueofDial + Amount;
+                }
+                Console.WriteLine(Password + "," + ValueofDial+","+LorR+","+Rotation);
+                if (ValueofDial == 0) Password++;
+            }
+            SR.Close();
+            Console.WriteLine(Password);
+        }
+    }
+}
