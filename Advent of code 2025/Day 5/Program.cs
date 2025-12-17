b0c5a0c6fce68c70e1878f55a44c179e59ecaa72
Daniel White <danielwhite1722@gmail.com>
2025-12-17 10:34:29 +0000

    Day5 part 2

--- Advent of code 2025/Day 5/Program.cs
+++ Advent of code 2025/Day 5/Program.cs
@@ -12,7 +12,7 @@ namespace Day_5
     {
         static void Main(string[] args)
         {
-            Day5_part1.start();
+            Day5_part2.start();
         }
         public static string GetFileContence()
         {
@@ -48,6 +48,45 @@ namespace Day_5
             return false;
         }
     }
-    
+    static public class Day5_part2
+    {
+        public static void start()
+        {
+            string input = Program.GetFileContence();
+            string[] TempStringArray = input.Split(' ');
+            string[] Ranges = TempStringArray[0].Split('\n');
+            string[] Food = TempStringArray[1].Split('\n');
+
+            ulong TotalFood = ulong.Parse(Ranges[0].Split('-')[1]) - ulong.Parse(Ranges[0].Split('-')[0]) +1;
+            List<ulong[]> UsedRanges = new List<ulong[]>();
+
+            ulong RangeCount =0;
+
+            for (int i = 0; i < Ranges.Length; i++)
+            {
+                if (Ranges[i] != "")
+                {
+                    ulong FirstValule = ulong.Parse(Ranges[i].Split('-')[0]);
+                    ulong SecondValue = ulong.Parse(Ranges[i].Split('-')[1]);
+                    
+                }
+            }
+
+
+            Console.WriteLine(RangeCount);
+            
+        }
+        static private bool IsInaRage(string input, string[] Ranges,int upto)
+        {
+            ulong Id = ulong.Parse(input);
+            for (int i = 0;i<Ranges.Length && i < upto;i++)
+            {
+                string s = Ranges[i];
+                if (s != "" && Id >= ulong.Parse(s.Split('-')[0]) && Id <= ulong.Parse(s.Split('-')[1])) return true;
+            }
+            return false;
+        }
+    }
+
 
 }
